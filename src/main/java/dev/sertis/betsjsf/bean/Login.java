0e281e8d424ab72df386f6c70528d996d2958be7
gomezbc <77118356+gomezbc@users.noreply.github.com>
2023-11-17 10:25:43 +0100

    Login bean

---
+++ src/main/java/dev/sertis/betsjsf/bean/Login.java
@@ -0,0 +1,50 @@
+package dev.sertis.betsjsf.bean;
+
+import businessLogic.BLFacade;
+import businessLogic.BLFacadeLocalImplementation;
+import domain.User;
+import exceptions.UserDoesntExist;
+
+public class Login {
+
+    private String dni;
+
+    private String passwd;
+
+    private BLFacade facade = new BLFacadeLocalImplementation();
+
+    public Login() {
+    }
+
+    public String checkCredentials(){
+        if (dni==null || dni.isEmpty() || passwd==null || passwd.isEmpty()){
+            return "error";
+        }
+        try {
+            User user = facade.getUser(dni);
+            if (user.checkCredentials(passwd)){
+                return "ok";
+            } else {
+                return "error";
+            }
+        } catch (UserDoesntExist e) {
+            return "error";
+        }
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getPasswd() {
+        return passwd;
+    }
+
+    public void setPasswd(String passwd) {
+        this.passwd = passwd;
+    }
+}
