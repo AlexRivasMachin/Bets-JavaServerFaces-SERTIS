d5818b0fc8c20c99ebfb0386801f945b42446ba0
gomezbc <77118356+gomezbc@users.noreply.github.com>
2023-11-22 11:23:10 +0100

    Solucionado bug en el cual la label al recargar la pagina volvia a su sitio y añadidos mensajes de error rojos para los validators

--- src/main/webapp/resources/js/login.js
+++ src/main/webapp/resources/js/login.js
@@ -1,14 +1,26 @@
-document.querySelectorAll('.form-input').forEach((inputWrapper) => {
-    const input = inputWrapper.querySelector('input');
-    const label = inputWrapper.querySelector('label');
+function ifInputHasValueShrinkLabel(){
+    updateFormInputs();
+}
 
-    input.addEventListener('focus', () => {
-        label.classList.add('label-shrink');
-    });
+function updateFormInputs(){
+    document.querySelectorAll('.form-input').forEach((inputWrapper) => {
+        const input = inputWrapper.querySelector('input');
+        const label = inputWrapper.querySelector('label');
 
-    input.addEventListener('blur', () => {
-        if (input.value === '') {
-            label.classList.remove('label-shrink');
+        if (input.value !== '') {
+            label.classList.add('label-shrink');
         }
+
+        input.addEventListener('focus', () => {
+            label.classList.add('label-shrink');
+        });
+
+        input.addEventListener('blur', () => {
+            if (input.value === '') {
+                label.classList.remove('label-shrink');
+            }
+        });
     });
-});
+}
+
+updateFormInputs();
