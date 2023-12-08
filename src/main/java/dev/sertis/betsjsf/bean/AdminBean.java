2b950a33e3741e49d38ef6265d67a1cbf652d399
gomezbc <77118356+gomezbc@users.noreply.github.com>
2023-12-08 11:12:55 +0100

    He dejado mas clean el bean del admin y cambiado los nombres de los metodos al english

--- src/main/java/dev/sertis/betsjsf/bean/AdminBean.java
+++ src/main/java/dev/sertis/betsjsf/bean/AdminBean.java
@@ -4,8 +4,7 @@ package dev.sertis.betsjsf.bean;
 
 public class AdminBean {
     public AdminBean() {
-        System.out.println("AdminBean created");
-        componentPath = "adminCerrarEventos.xhtml";
+        componentPath = "adminUIComponents/adminCerrarEventos.xhtml";
     }
     private String componentPath;
 
@@ -13,24 +12,20 @@ public class AdminBean {
         return componentPath;
     }
 
-    public void cambiarComponenteAnadirPreguntasYPronosticos(){
-        System.out.println("cambiando a anadir preguntas y pronosticos");
-        cambiarComponente("adminAnadirPreguntasYPronosticos.xhtml");
+    public void changeComponentToAddQuestionsAndForecasts(){
+        setComponentPath("adminUIComponents/adminAnadirPreguntasYPronosticos.xhtml");
     }
-    public void cambiarComponenteCerrarEventos(){
-        System.out.println("cambiando a cerrar evento");
-        cambiarComponente("adminCerrarEventos.xhtml");
+    public void changeComponentToCloseEvents(){
+        setComponentPath("adminUIComponents/adminCerrarEventos.xhtml");
     }
-    public void cambiarComponenteCrearEvento(){
-        System.out.println("cambiando a crear evento");
-        cambiarComponente("adminCrearEvento.xhtml");
+    public void changeComponentToCreateEvent(){
+        setComponentPath("adminUIComponents/adminCrearEvento.xhtml");
     }
-    public void cambiarComponenteListeDeEventos(){
-        System.out.println("cambiando a lista de eventos");
-        cambiarComponente("adminListaDeEventos.xhtml");
+    public void changeComponentToEventsList(){
+        setComponentPath("adminUIComponents/adminListaDeEventos.xhtml");
     }
 
-    public void cambiarComponente(String path){
+    private void setComponentPath(String path){
         componentPath = path;
     }
 
