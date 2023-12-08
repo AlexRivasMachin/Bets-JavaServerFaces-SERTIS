380a7a352c009dda1c4e12149defa868694f666c
AlexRivasMachin <arivas028@ikasle.ehu.eus>
2023-12-08 12:10:12 +0100

    he pasado a don limpio

--- src/main/java/dev/sertis/betsjsf/bean/UserBean.java
+++ src/main/java/dev/sertis/betsjsf/bean/UserBean.java
@@ -4,14 +4,6 @@ public class UserBean {
 
     private String username;
     private double saldo;
-    private boolean mostrarEventos = false;
-    private boolean mostrarApuestas = false;
-    private boolean mostrarAñadirSaldo = false;
-    private boolean mostrarDatosPersonales = false;
-    private boolean eventosButtonEnabled = true;
-    private boolean apuestasButtonEnabled = true;
-    private boolean añadirSaldoButtonEnabled = true;
-    private boolean datosPersonalesButtonEnabled = true;
     private String componentContent;
 
     public UserBean() {
@@ -25,33 +17,21 @@ public class UserBean {
     }
 
     public String showEventos() {
-        mostrarEventos = true;
-        resetFlagsExcept("mostrarEventos");
-        eventosButtonEnabled = false;
         componentContent = "userNormalComponentes/eventosComponente.xhtml";
         return null;
     }
 
     public String showApuestas() {
-        mostrarApuestas = true;
-        resetFlagsExcept("mostrarApuestas");
-        apuestasButtonEnabled = false;
         componentContent = "userNormalComponentes/apuestasRealizadasComponente.xhtml";
         return null;
     }
 
     public String showAñadirSaldo() {
-        mostrarAñadirSaldo = true;
-        resetFlagsExcept("mostrarAñadirSaldo");
-        añadirSaldoButtonEnabled = false;
         componentContent = "userNormalComponentes/añadirSaldoComponente.xhtml";
         return null;
     }
 
     public String showDatosPersonales() {
-        mostrarDatosPersonales = true;
-        datosPersonalesButtonEnabled = false;
-        resetFlagsExcept("mostrarDatosPersonales");
         componentContent = "userNormalComponentes/datosPersonalesComponente.xhtml";
         return null;
     }
@@ -64,13 +44,6 @@ public class UserBean {
         return saldo;
     }
 
-    private void resetFlagsExcept(String flagToKeep) {
-        if (!"mostrarEventos".equals(flagToKeep)) mostrarEventos = false;
-        if (!"mostrarApuestas".equals(flagToKeep)) mostrarApuestas = false;
-        if (!"mostrarAñadirSaldo".equals(flagToKeep)) mostrarAñadirSaldo = false;
-        if (!"mostrarDatosPersonales".equals(flagToKeep)) mostrarDatosPersonales = false;
-    }
-
     public String getComponentContent() {
         return componentContent;
     }
@@ -78,6 +51,4 @@ public class UserBean {
     public void setComponentContent(String componentContent) {
         this.componentContent = componentContent;
     }
-
-
 }
